59aaad56464660d39be8de01c7429d8b09e9be60
Duy Tran <tranducduy032@gmail.com>
2020-12-02 17:34:50 +1030

    Added static folder and show message in console

--- server.js
+++ server.js
@@ -2,19 +2,27 @@
 
 const express = require('express');
 const { Server } = require('ws');
+var path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
+const app = express()
+  .use(express.static(path.join(__dirname, 'public')))
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
+
+  
+const wss = new Server({ server: app });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
 });
 
 setInterval(() => {
