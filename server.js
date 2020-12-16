343a6da2681b9a7031684596b22ed5a92063aab5
David Milanese <milanese.david@gmail.com>
2020-12-16 22:10:46 +1030

    Re style the webpage. Added boostrap 4. Use constants. Send payload as a stingified JSON object

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const { Server } = require('ws');
 var path = require('path');
+const { START, STOP, DRAWING_STARTED, DRAWING_STOPPED, DRAWING } = require('./public/constants');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -16,41 +17,53 @@ const app = express()
 
 const wss = new Server({ server: app });
 
-var dataUrl;
+let dataUrl, start;
+
+const init = () => {
+  dataUrl = '';
+  wss.clients.forEach((client) => client.send(JSON.stringify({command: DRAWING, payload: dataUrl })));
+
+  start = false;
+  wss.clients.forEach((client) => client.send(JSON.stringify({ command: DRAWING_STOPPED })));
+
+  console.log('Server initialized')
+}
+
+init();
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-  let start = false
-  ws.on('message', function incoming(message) {
-    if (message === 'Start' && start == false){
-      start = true
-      console.log(message)
-      wss.clients.forEach((client) => {
-        client.send('Disable start');
-      });
-      return
-    }
-    if (message === 'Start' && start == true){
-      console.log('Already started')
-      return
-    }
-
-    if (message === 'Stop' && start == true){
-      start = false
-      console.log(message)
-      return
-    }
-    if (message === 'Stop' && start == false){
-      console.log('Hasnt started')
-      return
-    }
-    dataUrl = message;
+  
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    init();
   });
 
+  ws.on('message', (message) => {
+    switch(message) {
+      case START:
+        if (start) {
+          console.log('Already started')
+          break;
+        }
+        
+        start = true
+        console.log('Start Drawing')
+        wss.clients.forEach((client) => client.send(JSON.stringify({command: DRAWING_STARTED})));
+        break;
+
+      case STOP:
+        if (!start) {
+          console.log("Drawing hasn't started yet")
+          break;
+        }
+
+        start = false
+        console.log('Stop drawing')
+        wss.clients.forEach((client) => client.send(JSON.stringify({command: DRAWING_STOPPED})));
+        break;
+      default:
+        wss.clients.forEach((client) => client.send(JSON.stringify({command: DRAWING, payload: message})));
+    }
+  });
 });
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(dataUrl);
-  });
-}, 100);
