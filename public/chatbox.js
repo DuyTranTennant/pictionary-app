f554282f036d8946df8d336349479b8044adc03f
Duy Tran <tranducduy032@gmail.com>
2021-01-06 16:14:44 +1030

    Added chatbox on client

---
+++ public/chatbox.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+    let HOST = location.origin.replace(/^http/, 'ws')
+    let ws = new WebSocket(HOST);
+    const toggleChatboxBtn = document.querySelector(".js-chatbox-toggle");
+    const chatbox = document.querySelector(".js-chatbox");
+    const chatboxMsgDisplay = document.querySelector(".js-chatbox-display");
+    const chatboxForm = document.querySelector(".js-chatbox-form");
+
+    // Use to create chat bubble when user submits text
+    // Appends to display
+    const createChatBubble = input => {
+        ws.send(JSON.stringify({command: constants.CHAT, message: input}));
+        const chatSection = document.createElement("p");
+        chatSection.textContent = input;
+        chatSection.classList.add("chatbox__display-chat");
+
+        chatboxMsgDisplay.appendChild(chatSection);
+        };
+
+        // Toggle the visibility of the chatbox element when clicked
+        // And change the icon depending on visibility
+        toggleChatboxBtn.addEventListener("click", () => {
+        chatbox.classList.toggle("chatbox--is-visible");
+
+        if (chatbox.classList.contains("chatbox--is-visible")) {
+            toggleChatboxBtn.innerHTML = '<i class="fas fa-chevron-down"></i>';
+        } else {
+            toggleChatboxBtn.innerHTML = '<i class="fas fa-chevron-up"></i>';
+        }
+    });
+
+    // Form input using method createChatBubble
+    // To append any user message to display
+    chatboxForm.addEventListener("submit", e => {
+        const chatInput = document.querySelector(".js-chatbox-input").value;
+
+        createChatBubble(chatInput);
+
+        e.preventDefault();
+        chatboxForm.reset();
+    });
+})
